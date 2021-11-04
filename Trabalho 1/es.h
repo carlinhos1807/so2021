348b18af2ae590413242f13db511ceda9881dc7c
carlinhos1807 <48457630+carlinhos1807@users.noreply.github.com>
2021-11-04 15:44:17 -0300

    Delete es.h

--- Trabalho 1/es.h
+++
@@ -1,9 +0,0 @@
-#include "memoria.h"
-
-typedef struct es es_t;
-
-err_t es_le(es_t *es, int dispositivo, int *pvalor);
-err_t es_escreve(es_t *es, int dispositivo, int valor);
-es_t* es_cria();
-void es_destroi(es_t *es);
-
