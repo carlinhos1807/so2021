b977dca023041f46584d2ed32fd898fd4691e607
carlinhos1807 <48457630+carlinhos1807@users.noreply.github.com>
2021-11-04 15:44:12 -0300

    Delete err.h

--- Trabalho 1/err.h
+++
@@ -1,8 +0,0 @@
-typedef enum{
-    ERR_OK = 0,
-    ERR_MEM_END_INV,
-    ERR_ES_DISP_INV,  // acesso a dispositivo inexistente
-    ERR_ES_OP_INV,    // operação inválida em dispositivo
-    ERR_CPU_PARADA,    // CPU executou instrução PARA
-    ERR_CPU_INSTR_INV // tentativa de execução de instrução inexistente
-} err_t;
