c0bc0e750b2ee75cec19b1fcb17abe577cf9083c
carlinhos1807 <48457630+carlinhos1807@users.noreply.github.com>
2022-01-26 03:54:42 -0300

    Add files via upload

---
+++ Trabalho 4/RR/uc.h
@@ -0,0 +1,20 @@
+#ifndef UC_H_INCLUDED
+#define UC_H_INCLUDED
+
+#include "cpu.h"
+#include "so.h"
+#include "relogio.h"
+#include "err.h"
+
+typedef struct uc uc_t;
+
+uc_t* uc_cria();
+void executa(uc_t *uc);
+err_t controla(uc_t *uc, cpu_t *cpu);
+void uc_altera_cpu(uc_t *uc, cpu_t* cpu);
+void uc_altera_relogio(uc_t *uc, relogio_t* relogio);
+void uc_altera_so(uc_t *uc, so_t *so);
+void uc_libera(uc_t *uc);
+void verifica_desbloqueios(uc_t *uc, relogio_t* relogio);
+
+#endif // UC_H_INCLUDED
